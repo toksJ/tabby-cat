ffd2da3b9cd09fd729fa2bb8f4e574ff9cc42c5b
toksJ <tokajaber11@gmail.com>
2023-09-08 18:17:06 +0400

    Initial Commit

---
+++ tabby cat/tabby_catApp.swift
@@ -0,0 +1,17 @@
+//
+//  tabby_catApp.swift
+//  tabby cat
+//
+//  Created by TokaJaber on 08/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct tabby_catApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
